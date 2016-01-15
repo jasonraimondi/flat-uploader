b765da7bfff8eea8b110a9506e4590ece0ed6b09
Jason Raimondi <jason@raimondi.us>
2016-01-15 11:49:00 -0500

    forget to add comments until after I submit the project, check!

--- dist/functions/delete.php
+++ dist/functions/delete.php
@@ -1,10 +1,17 @@
 <?php
 
-if (isset($_POST['filename']) && file_exists ($_POST['filename'])) {
+// Make sure the filename post var is set and make sure that file actually exists
+if (isset($_POST['filename']) && file_exists($_POST['filename'])) {
+
+  // if it does exist, delete it
   unlink($_POST['filename']);
   echo '<div class="notifications success"><p>Success! The File has been deleted.</p></div>';
+
 } else {
+
+  // if it doesn't exist, yell at the user.
   echo '<div class="notifications alert"><p>I can\'t find the file you wan\'t to delete. Please try again.</p></div>';
+
 }
 
 header("Location: ../#myfiles");
--- dist/functions/get.php
+++ dist/functions/get.php
@@ -9,21 +9,28 @@ class FileList {
   public $files = [];
 }
 
+// create new FileList object to serve as collection of files
 $filelist = new FileList();
 
+// if you can find and open the directory
 if ($handle = opendir('../uploads')) {
-  while (false !== ($system_filename = readdir($handle))) {
-    if ($system_filename != '.' && $system_filename != '..') {
 
+  // loop through the ../uploads directory and generate File objects for
+  // each file, then add the file objec to a FileList collection of files
+  while (false !== ($system_filename = readdir($handle))) {
+
+    // don't count the current directory and the previous directory
+    if ($system_filename != '.' && $system_filename != '..') {
       $file = new File();
       $file->filename = $system_filename;
       $file->uploaded = date('F d Y H:i:s.', filemtime('../uploads/'.$system_filename));
       $filelist->files[] = $file;
-
     }
   }
+  // close out the directory
   closedir($handle);
 }
 
+// set the content type to output JSON
 header('Content-Type: application/json');
 echo json_encode($filelist);
